a61a29d35aba53f90bb2ac89bae964495bc795b3
hrmtk <hirohrmtk@gmail.com>
2023-07-02 00:11:47 +0900

    MODIFY: default app

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,69 +20,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  String text = 'next';
-  final myFocusNode = FocusNode();
-  final myController = TextEditingController();
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('coriander'),
-        actions: <Widget>[Icon(Icons.add), Icon(Icons.share)],
+        title: Text('本一覧'),
       ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-              hintText: 'name',
-            ),
-            
-            ),
-            TextField(
-              controller: myController,
-              decoration: InputDecoration(
-              hintText: 'email',
-              ),
-            ),
-            FloatingActionButton(
-              child: Text('sign up'),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      content: Text(myController.text),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
+      body: Center(
+        child: Text(
+          'You have pushed the button this many times',
         ),
       ),
     );
   }
 }
+
+
